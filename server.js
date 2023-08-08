557d5ea1dc5dc3f3f0800759070662da7d7319b0
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-08-08 23:24:14 +0300

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  'mongodb+srv://kostiantyn:AgAinNewPass@learningcluster.jftkfqm.mongodb.net/?retryWrites=true&w=majority';
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log(
+        'If you see this message - it means MongoDB is conected. Server running. Use our API on port: 3000'
+      );
+    })
+  )
+  .catch(error => console.log(error.message));
